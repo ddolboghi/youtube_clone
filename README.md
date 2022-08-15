294c7caf475d9059074f2f62357e968261c617ca
JaeYoon Jeong <rhdfyd128@gmail.com>
2022-08-15 10:53:26 +0900

    main 페이지 구현

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # youtube_clone
 유튜브 클론 코딩
+
+# main 앱
+- youtube 첫 화면 구성
+- model 만들기부터 시작
---
+++ main/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from .models import Video
+
+def video_list(request):
+  video_list = Video.objects.all()
+  return render(request, 'video/video_list.html', {'video_list': video_list})
