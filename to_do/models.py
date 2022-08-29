6ce2a4dc69a511efaba8468715c4818ca264e613
JaeYoon Jeong <rhdfyd128@gmail.com>
2022-08-29 15:17:26 +0900

    author 필드 추가

--- to_do/models.py
+++ to_do/models.py
@@ -1,13 +1,17 @@
 from time import timezone
+from tkinter import CASCADE
 from django.db import models
+from django.contrib.auth.models import User
 
 class ToDo(models.Model):
   content = models.TextField()
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
   
+  author = models.ForeignKey(User, on_delete=models.CASCADE)
+  
   def __str__(self):
-    return f'To do({self.pk})'
+    return f'To do({self.pk}) :: {self.author}'
   
   def get_absolute_url(self):
     return f'/to_do/{self.pk}/'
