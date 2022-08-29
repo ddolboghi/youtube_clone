b76a6513d22316c5b255d02fda4cac9daa082f9c
JaeYoon Jeong <rhdfyd128@gmail.com>
2022-08-29 15:48:21 +0900

    to do 페이지에 작성자 표시

--- to_do/tests.py
+++ to_do/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from bs4 import BeautifulSoup
+from .models import ToDo
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class TestView(TestCase):
+  def setUp(self):
+    self.client = Client()
+    self.user_test1 = User.objects.create_user(username='test1', password='somepassword')
+    self.user_test2 = User.objects.create_user(username='test2', password='somepassword')
