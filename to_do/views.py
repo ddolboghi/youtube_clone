c59c8da608c56fbb56d7fd0d254fa8f894ed8e23
JaeYoon Jeong <rhdfyd128@gmail.com>
2022-08-31 17:08:54 +0900

    로그인 유저만 작성 & 삭제되도록 수정

--- to_do/views.py
+++ to_do/views.py
@@ -2,15 +2,24 @@ from django.shortcuts import get_object_or_404, redirect
 from .models import ToDo
 from django.views.generic import ListView, CreateView
 from .form import ToDoForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class ToDoList(ListView):
   model = ToDo
   ordering = '-pk'
 
-class ToDoNew(CreateView):
+class ToDoNew(LoginRequiredMixin, CreateView):
   form_class = ToDoForm
   success_url = '/to_do'
   template_name = 'to_do/new_todo.html'
+  
+  def form_valid(self, form):
+    current_user = self.request.user
+    if current_user.is_authenticated:
+      form.instance.author = current_user
+      return super(ToDoNew, self).form_valid(form)
+    else:
+      return redirect('/to_do/')
   
 def delete_todo(request, pk):
   todo = get_object_or_404(ToDo, pk=pk)
